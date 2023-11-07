3466203ce09cb77d860f3133e4162b56bffc474e
Dmitry Fisenko <dfisenko94@gmail.com>
2023-11-07 20:29:44 +0700

    small improvment

--- src/main/java/ru/fisenko/urlRestApp/controllers/UrlController.java
+++ src/main/java/ru/fisenko/urlRestApp/controllers/UrlController.java
@@ -2,26 +2,19 @@ package ru.fisenko.urlRestApp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.fisenko.urlRestApp.services.UrlService;
 
 @RestController
-@RequestMapping("/api")
 public class UrlController {
-    private UrlService urlService;
+    private final UrlService urlService;
     @Autowired
     public UrlController(UrlService urlService) {
         this.urlService = urlService;
     }
 
-    @GetMapping("/url")
+    @GetMapping("/generate")
     public String newUrl (){
         return urlService.getUrl();
     }
-
-
-
-
 }
